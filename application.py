6bac5128d2019b56bf21e4ffc3c3a56129e85d44
maturiabhinav <abhinav.m@futureinvo.com>
2025-11-12 15:27:49 +0530

    update

--- application.py
+++ application.py
@@ -1,9 +1,14 @@
 from app import create_app
 import os
+from flask import redirect, url_for
 
 app = create_app()
+
+# Add root route at application level
+@app.route('/')
+def home():
+    return redirect(url_for('auth.login'))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
-

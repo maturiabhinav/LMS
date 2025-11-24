92f55791a74b966197415abdca3b9ee72f46240f
maturiabhinav <abhinav.m@futureinvo.com>
2025-11-24 18:57:55 +0530

    update

--- app/models.py
+++ app/models.py
@@ -42,6 +42,11 @@ class User(UserMixin, db.Model):
 
     tenant = db.relationship("Tenant", back_populates="users")
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def get_role(self):
         return self.role.value
